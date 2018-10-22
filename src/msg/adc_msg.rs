763391fbcea1324e08778cb9bb984f4eace52a8f
Gu Junhua <jhgu@nao.cas.cn>
2018-10-22 23:34:26 +0800

    modified:   src/msg/adc_msg.rs

--- src/msg/adc_msg.rs
+++ src/msg/adc_msg.rs
@@ -1,14 +1,29 @@
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+pub enum CtrlParam{
+    Sync,
+    PreRst,
+    StartFft,
+    SwitchPhaseFactor,
+    StoreData,
+}
+
+
+#[derive(Debug, Clone, Eq, PartialEq)]
 pub enum AdcMsg{
-    Trig(u8),
+    MasterSync,
+    MasterRst,
+    MasterTrig,
+    Ctrl(CtrlParam),
     Cfg{
         io_delay:[u8;4],
         packet_gap:u16,
         counter_sync:u8,
         counter_wait:u16,
         trig_out_delay:u8,
+        optical_delay:u8,
     },
     StartDaq,
-    StartFft,
+    UploadFft,
     FftParam{
         fft_shift:u16,
         truncation:u32,
@@ -22,10 +37,13 @@ pub enum AdcMsg{
 impl AdcMsg{
     pub fn msg_type_code(&self)->u8{
         match self{
-            &AdcMsg::Trig(..)=>0xf3,
+            &AdcMsg::MasterSync=>0xf0,
+            &AdcMsg::MasterRst=>0xf1,
+            &AdcMsg::MasterTrig=>0xf2,
+            &AdcMsg::Ctrl(..)=>0xf3,
             &AdcMsg::Cfg {..}=>0xf4,
             &AdcMsg::StartDaq=>0xf5,
-            &AdcMsg::StartFft=>0xf6,
+            &AdcMsg::UploadFft=>0xf6,
             &AdcMsg::FftParam {..}=>0xf8,
             &AdcMsg::PhaseFactor {..}=>0xf9,
             &AdcMsg::QueryState{..}=>0xfa,
