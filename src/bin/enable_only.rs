aa45876d0af4f0c6b5dd60fa261d3ef2f1ee1b69
Gu Junhua <jhgu@nao.cas.cn>
2018-11-08 01:06:57 +0800

    new file:   src/bin/enable_only.rs

---
+++ src/bin/enable_only.rs
@@ -0,0 +1,41 @@
+extern crate controller_rs;
+extern crate num_complex;
+extern crate pcap;
+extern crate serde_yaml;
+
+use std::io::Read;
+use std::fs::File;
+use std::str;
+use std::env;
+use serde_yaml::{Value, from_str};
+
+
+use num_complex::Complex;
+use pcap::{Capture, Device};
+
+use controller_rs::board_cfg::BoardCfg;
+
+
+fn main(){
+    let mut cap = Capture::from_device(Device {
+        name: env::args().nth(1).expect("iface name not found").to_string(),
+        desc: None,
+    }).unwrap()
+        .open()
+        .unwrap();
+
+    let bid:usize=env::args().nth(3).expect("bid miss").parse().expect("bad bid");
+    let pid:usize=env::args().nth(4).expect("pid miss").parse().expect("bad pid");
+
+    let mut fparam = File::open(env::args().nth(2).unwrap()).unwrap();
+    let mut bytes = Vec::new();
+    fparam.read_to_end(&mut bytes).expect("Cannot read file");
+    let msg_str = str::from_utf8(&bytes).unwrap().to_string();
+    let param = from_str::<Value>(&msg_str).expect("Unable to read param");
+    let bc = BoardCfg::from_yaml(&param);
+
+    let mut pf=vec![vec![vec![Complex::<i16>::new(0,0);2048];8];16];
+    pf[bid][pid]=vec![Complex::<i16>::new(1,0);2048];
+
+    bc.update_phase_factor(&mut cap, pf, [0x11, 0x22, 0x33, 0x44, 0x55, 0x66]);
+}
