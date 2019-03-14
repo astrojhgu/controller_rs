ca707a4b73e6292cfe2513af0a60063972259631
Junhua GU <jhgu@nao.cas.cn>
2019-03-14 23:57:59 +0800

    修改：     py/watch_spec.py

--- py/watch_spec.py
+++ py/watch_spec.py
@@ -22,7 +22,15 @@ def hide_tick_labels(ax, hide_x=False, hide_y=False):
         ticks += ax.get_yticklabels()
     for tl in ticks:
         tl.set_visible(False)
-                
+
+
+
+def calc_corr(fft_data, ref_port_id):
+    result=np.zeros_like(fft_data, dtype='complex')
+    for i in range(0, fft_data.shape[0]):
+        #print("calculating port {0}".format(i))
+        result[i,:]=fft_data[i, :]*np.conj(fft_data[ref_port_id, :])
+    return result
 
 
 dev_name=sys.argv[1]
@@ -31,7 +39,7 @@ cfg_name=sys.argv[2]
 
 p = pcap.pcapObject()
 
-p.open_live(dev_name, 1600, True , 0)
+p.open_live(dev_name, 1600, True , 100)
 
 cfg=yaml.load(open(cfg_name))
 
@@ -39,7 +47,11 @@ mac_list=cfg['mac']
 
 mac_idx_map={tuple(x[1]):x[0] for x in enumerate(mac_list)} 
 
-fft_data=np.array([[0. for i in range(0,2048)] for i in range(len(mac_idx_map)*8)])
+spec_data=np.array([[0. for i in range(0,2048)] for i in range(len(mac_idx_map)*8)])
+fft_data=np.array([[0.+0.0j for i in range(0,2048)] for i in range(len(mac_idx_map)*8)])
+corr_data=np.array([[0.+0.0j for i in range(0,2048)] for i in range(len(mac_idx_map)*8)])
+
+
 
 update_ratio=0.9
 
@@ -50,17 +62,24 @@ request=subprocess.Popen(["../target/release/fetch_fft", "enp3s0", "../param1.ya
 
 cnt=0
 
-print(fft_data.shape)
+print(spec_data.shape)
 try:
     while True:
-        pktlen, data, timestamp=next(p)
+        pkt=next(p)
+        if pkt is None:
+            print("pkt missed, send request again")
+            request.wait()
+            request=subprocess.Popen(["../target/release/fetch_fft", "enp3s0", "../param1.yaml"])
+            continue
+        pktlen, data, timestamp=pkt
         raw_data=bytearray(data)
         src_mac=tuple(i for i in raw_data[6:12])
         if src_mac in mac_idx_map and pktlen==1024+12+2+4+1:
             bid=mac_idx_map[src_mac]
             data_order=struct.unpack('=B',data[18])[0]-1
             fft_data1=np.array(struct.unpack('=256i', data[19:]))
-            fft_data1=fft_data1[::2]+1.j*fft_data1[1::2]
+            fft_data1=1.j*fft_data1[::2]+fft_data1[1::2]
+            
             ps=np.real(fft_data1*np.conj(fft_data1))
             
             chip_id=int(data_order/16)
@@ -69,17 +88,21 @@ try:
             ch1=(data_order-chip_id*16)*128
             ch2=ch1+128
 
-            #fft_data[port_id,ch1:ch2]=fft_data[port_id, ch1:ch2]*update_ratio+(1.-update_ratio)*ps
-            fft_data[port_id,ch1:ch2]+=ps
+            #spec_data[port_id,ch1:ch2]=spec_data[port_id, ch1:ch2]*update_ratio+(1.-update_ratio)*ps
+            spec_data[port_id,ch1:ch2]+=ps
+            fft_data[port_id, ch1:ch2]=fft_data1
             #print "%d %s %d %d" % ( pktlen, src_mac, bid, data_order)
 
             if bid==15 and data_order==127:
                 request.wait()
+                
+                corr_data=corr_data*update_ratio+(1.-update_ratio)*calc_corr(fft_data, 0)
+                
                 cnt+=1
                 if cnt%10==0:
                     print("ploting")
-                    ymax=10*np.max(np.log10(fft_data[fft_data>0]))
-                    ymin=10*np.min(np.log10(fft_data[fft_data>0]))
+                    ymax=10*np.max(np.log10(spec_data[spec_data>0]))
+                    ymin=10*np.min(np.log10(spec_data[spec_data>0]))
                     dy=ymax-ymin
                     ymax+=dy*0.1
                     ymin-=dy*0.1
@@ -97,13 +120,38 @@ try:
                             hide_tick_labels(ax, hide_x=True, hide_y=True)
                             ax.set_ylim(ymin,ymax)
                             ax.set_xlim(-1, 2050)
-                            ax.plot(10*np.log10(fft_data[port_id,:]))
+                            ax.plot(10*np.log10(spec_data[port_id,:]))
 
                     plt.tight_layout()
-                    plt.savefig('a.png')
-                    print("fig saved")
-                    fft_data*=0.0;
-                    time.sleep(5)
+                    plt.savefig('auto.png')
+                    print("fig1 saved")
+                    spec_data*=0.0;
+
+                    plt.close()
+
+                    ymax=max(np.max(corr_data.real),np.max(corr_data.imag))
+                    ymin=min(np.min(corr_data.real),np.min(corr_data.imag))
+                    ymax*=1.1
+                    ymin*=1.1
+                    ymax=max(ymax, abs(ymin))
+                    ymin=-ymax
+                    
+                    fig=plt.figure(figsize=(40,20))
+                    for j in range(16):
+                        print("ploting board {0}".format(j))
+                        for i in range(8):
+                            bid=j
+                            cid=i
+                            port_id=bid*8+cid
+                            ax.set_ylim(ymin, ymax)
+                            ax=plt.subplot(gs[i,j])
+                            hide_tick_labels(ax, hide_x=True, hide_y=True)
+                            ax.plot(corr_data[port_id,:].real)
+                            ax.plot(corr_data[port_id,:].imag)
+                    plt.tight_layout()
+                    plt.savefig('corr.png')
+                    print("fig2 saved")
+                    time.sleep(1)
                 request=subprocess.Popen(["../target/release/fetch_fft", "enp3s0", "../param1.yaml"])
         
 except KeyboardInterrupt:
